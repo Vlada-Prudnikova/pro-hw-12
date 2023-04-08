4011a2f2a82748a12bf36afcefe2127525227900
git-pages <vlada.prudnikova.2004@gmail.com>
2023-04-08 02:16:36 -0400

    second commit

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
 let array = [1, 2, 3, 4, 5, 6, 7];
-const index = array.indexOf(2);
+//const index = array.indexOf(2);
 function removeElement(array) {
-  delete array[4];
+  array.splice(4, 1);
   return array;
 }
 console.log(removeElement(array));
